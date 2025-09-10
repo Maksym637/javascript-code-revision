b711357bbd33fa744a606b4b1110ed377a5a1730
moliinyk <maks.oliynik228@gmail.com>
2025-09-10 23:28:13 +0200

    Updated lesson 62: (Added chaining promises)

---
+++ 062_lesson (promises and async await and ajax)/script3/script.js
@@ -0,0 +1,8 @@
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
+
+getCountryData("portugal");
