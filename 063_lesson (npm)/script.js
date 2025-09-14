2f5f907c14cb0d14e881aac8c1566668a70d5c91
moliinyk <maks.oliynik228@gmail.com>
2025-09-14 18:51:48 +0200

    Added lesson 63: NPM explanation and example with a cloneDeep function

---
+++ 063_lesson (npm)/script.js
@@ -0,0 +1,18 @@
+import { cloneDeep } from "lodash-es";
+
+const state = {
+  cart: [
+    { product: "bread", quantity: 5 },
+    { product: "pizza", quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+
+console.log(state);
+console.log(stateClone);
+console.log(stateDeepClone);
